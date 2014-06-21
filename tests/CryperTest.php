729e1eb8a7d12c17ae8da3fbc9b27621755a2e68
Sahan H <sahan@sahanz.com>
2014-06-21 16:58:06 +0530

    using mocked key classes

--- tests/CryperTest.php
+++ tests/CryperTest.php
@@ -3,12 +3,23 @@ use SZ\EZCash\Crypter;
 use SZ\EZCash\Key\PublicKey;
 use SZ\EZCash\Key\PrivateKey;
 
+use Mockery as m;
+
 class CrypterTest extends PHPUnit_Framework_Testcase
 {
     public function testEncryptDecrypt()
     {
-        $public_key  = new PublicKey(__DIR__.'/shared/public_key.key');
-        $private_key = new PrivateKey(__DIR__.'/shared/private_key.key');
+        $public_key = m::mock('SZ\EZCash\Key\PublicKey', array(__DIR__.'/shared/public_key.key'))
+                        ->shouldReceive('getFormattedKey')
+                        ->times(1)
+                        ->andReturn(file_get_contents(__DIR__.'/shared/public_key_formatted.key'))
+                        ->getMock();
+
+        $private_key = m::mock('SZ\EZCash\Key\PrivateKey', array(__DIR__.'/shared/private_key.key'))
+                        ->shouldReceive('getFormattedKey')
+                        ->times(1)
+                        ->andReturn(file_get_contents(__DIR__.'/shared/private_key_formatted.key'))
+                        ->getMock();
 
         $c = new Crypter;
         $c->setKey($public_key);
@@ -17,5 +28,10 @@ class CrypterTest extends PHPUnit_Framework_Testcase
         $c->setKey($private_key);
         
         $this->assertEquals('test', $c->process($enc));
+    }
+
+    public function tearDown()
+    {
+        m::close();
     }
 }
