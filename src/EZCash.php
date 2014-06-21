283aff3548347e37ad9b68570bc045a4c22c1677
Sahan H <sahan@sahanz.com>
2014-06-21 18:02:04 +0530

    implemented constructor

--- src/EZCash.php
+++ src/EZCash.php
@@ -4,10 +4,31 @@
  */
 namespace SZ\EZCash;
 
+use SZ\EZCash\Key\PrivateKey;
+use SZ\EZCash\Key\PublicKey;
+
 class EZCash
 {
     protected $endpoint = 'https://ipg.dialog.lk/ezCashIPGExtranet/servlet_sentinal';
     
+    /**
+     * Path to public key file
+     * @var string
+     */
+    protected $public_key;
+
+    /**
+     * Path to private key file
+     * @var string
+     */
+    protected $private_key;
+
+    public function __construct($public_key, $private_key)
+    {
+        $this->public_key  = new PublicKey($public_key);
+        $this->private_key = new PrivateKey($private_key);
+    }
+
     public function getEndpoint()
     {
         return $endpoint;
@@ -22,12 +43,12 @@ class EZCash
     {
         $req = new Request;
         $req->setMerchantCode($params['merchant']);
-        $req->setTransactionId($params['transaction']);
+        $req->setTransactionId($params['transaction_id']);
         $req->setAmount($params['amount']);
         $req->setReturnUrl($params['url']);
 
         $c = new Crypter;
-        $c->setKey(new Publickey('mypublicKey.key'));
+        $c->setKey($this->public_key);
         
         return $c->process($req);
     }
@@ -39,12 +60,14 @@ class EZCash
 
     public function getReceipt($response)
     {
-        $res = new Response($response);
-        $c = new Crypter;
+        $res     = new Response($response);
+        $c       = new Crypter;
         $c->setKey(new PrivateKey('mypublicKey.key'));
         
-        $data = $c->process($req);
-        
-        return new Receipt($data);
+        $data    = $c->process($req);
+        $receipt = new Receipt($data);
+        $receipt->validate();
+
+        return $receipt;
     }
 }
